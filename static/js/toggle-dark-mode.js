5f3b7c4198575ab00e013be66d864dae25373fb8
Romain Lesueur <lesueur221@campus.unicaen.fr>
2024-11-27 11:24:14 +0100

    Correctif sur le darkmode

--- static/js/toggle-dark-mode.js
+++ static/js/toggle-dark-mode.js
@@ -2,9 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const toggle = document.getElementById("dark-mode-toggle");
     const body = document.body;
     const table = document.querySelector("table");
+    const h1 = document.querySelector("h1");
+    const h2 = document.querySelector("h2");
 
     toggle.addEventListener("click", function () {
         body.classList.toggle("dark-mode");
+        h1.classList.toggle("dark-mode");
+        h2.classList.toggle("dark-mode");
         table.classList.toggle("dark-mode");
     });
 });
